4e74571ef26f818c82a8e77a879ae098b7e97b04
Zhuo FENG <zhuo.feng@student.unimelb.edu.au>
2019-10-04 01:23:55 +1000

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~/CarWash
+git fetch --all
+git reset --hard origin/master
+cd backend
+npm install
+pm2 restart all
+cd ../frontend
+npm install
+npm run build
+echo 'Deploy Successfully'
